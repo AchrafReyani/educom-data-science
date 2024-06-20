2cfa92ed377e304f3027df7369d838b3ebfa5aa0
Achraf Reyani <achrafreyani99@gmail.com>
2024-06-20 10:37:01 +0200

    (#5)

--- relational-data-storage/opdracht-4/DS-opdracht-4.2.3.sql
+++ relational-data-storage/opdracht-4/DS-opdracht-4.2.3.sql
@@ -1,1 +1,5 @@
-SELECT R2.id, IFNULL(R1.name, R2.name) as hoofdrubriek, IF(ISNULL(R1.name), '', R2.name) as subrubriek FROM mhl_rubrieken AS R1 RIGHT OUTER JOIN mhl_rubrieken AS R2 ON R1.id = R2.parent ORDER BY hoofdrubriek, subrubriek;
+SELECT R2.id, IFNULL(R1.name, R2.name) as hoofdrubriek, 
+IF(ISNULL(R1.name), '', R2.name) as subrubriek 
+FROM mhl_rubrieken AS R1 
+RIGHT OUTER JOIN mhl_rubrieken AS R2 ON R1.id = R2.parent 
+ORDER BY hoofdrubriek, subrubriek;
